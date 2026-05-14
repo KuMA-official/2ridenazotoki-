09e3be24d81b4617f8f3964cf004993aebcae662
KuMA-official <97272648+KuMA-official@users.noreply.github.com>
2026-05-14 13:22:18 +0900

    v0.0.1

--- Assets/Script/title.cs
+++ Assets/Script/title.cs
@@ -6,36 +6,57 @@ using UnityEngine.SceneManagement;
 
 public class title : MonoBehaviour
 {
+    // 二重押しを防止するためのフラグ
+    private bool isConnecting = false;
+
     // ホストとして開始
     public void OnClickHost()
     {
+        if (isConnecting) return; // 接続中なら何もしない
+        isConnecting = true;
+
         if (NetworkManager.Singleton.StartHost())
         {
-            // NGOのSceneManagerを使ってゲーム画面へ（Build Settingsに"Game"シーンが必要）
-            NetworkManager.Singleton.SceneManager.LoadScene("Game", UnityEngine.SceneManagement.LoadSceneMode.Single);
+            // NGOのSceneManagerを使ってゲーム画面へ
+            NetworkManager.Singleton.SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+        else
+        {
+            isConnecting = false; // 失敗したら再度押せるようにする
         }
     }
 
     // クライアントとして開始
     public void OnClickClient()
     {
-        NetworkManager.Singleton.StartClient();
+        if (isConnecting) return; // 接続中なら何もしない
+        isConnecting = true;
+
+        if (!NetworkManager.Singleton.StartClient())
+        {
+            isConnecting = false; // 失敗したら再度押せるようにする
+        }
     }
 
     private void Update()
     {
-        // QuestのAボタンでホスト開始テスト用
-        if(OVRInput.GetDown(OVRInput.RawButton.A))
+        // すでに接続済み、または接続処理中ならボタン入力を無視する
+        if (NetworkManager.Singleton.IsClient || NetworkManager.Singleton.IsServer || isConnecting)
+        {
+            return;
+        }
+
+        // QuestのAボタンでホスト開始
+        if (OVRInput.GetDown(OVRInput.RawButton.A))
         {
             OnClickHost();
         }
 
-        if(OVRInput.GetDown(OVRInput.RawButton.B))
+        // QuestのBボタンでクライアント開始
+        if (OVRInput.GetDown(OVRInput.RawButton.B))
         {
+            Debug.Log("Bボタン押された：接続を開始します");
             OnClickClient();
-            Debug.Log("Bぼたんおされた");
-
         }
     }
 }
-
